6054c3c9f1b9571bb4055e9f26330ce64f7fd5fe
Camille <camilledubois1995@gmail.com>
2025-05-20 01:09:51 -0400

    authors get and post methods

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -4,3 +4,34 @@ from .route_utilities import validate_model
 from ..db import db
 
 bp = Blueprint("bp", __name__, url_prefix="/authors")
+
+@bp.post("")
+def create_book():
+    request_body = request.get_json()
+    try:
+        new_author = Author.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return new_author.to_dict(), 201
+
+@bp.get("")
+def get_all_authors():
+    query = db.select(Author)
+
+    name_param = request.args.get("name")
+    if name_param:
+        query = query.where(Author.title.ilike(f"%{name_param}%"))
+
+    query = query.order_by(Author.id)
+    authors = db.session.scalars(query)
+
+    authors_response = []
+    for author in authors:
+        authors_response.append(author.to_dict())
+    return authors_response
