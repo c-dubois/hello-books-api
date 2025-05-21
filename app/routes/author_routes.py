402efac9bcc891e79df5ec65effb46c5e9ab6ec6
Camille <camilledubois1995@gmail.com>
2025-05-20 22:44:22 -0400

    finished models and routes

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -7,7 +7,7 @@ from ..db import db
 bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
 @bp.post("")
-def create_book():
+def create_author():
     request_body = request.get_json()
     return create_model(Author, request_body)
 
